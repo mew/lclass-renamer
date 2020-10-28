35fe905440874be6c00dce5e0f9a6d2af5de39fc
Nora <ncos@protonmail.ch>
2020-10-28 16:50:10 -0400

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+use std::fs::ReadDir;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_dir("./input").unwrap();
+    convert(input)
+}
+
+fn convert(directory: ReadDir) {
+    for file in directory {
+        let a = file.unwrap();
+        let s = a.path().display().to_string();
+        if a.file_name().to_str().unwrap().ends_with(".lclass") {
+            fs::rename(&s, &s.replace(".lclass", ".class"));
+            println!("Done")
+        } else if a.file_type().unwrap().is_dir() {
+            convert(fs::read_dir(&s).unwrap())
+        }
+    }
 }
